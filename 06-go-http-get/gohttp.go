69a58abdfb0d53c174ffce90676aeefc0580bcb9
tangx <uyinn@live.com>
2020-01-05 23:51:13 +0800

    实现自定义http 请求

--- 06-go-http-get/gohttp.go
+++ 06-go-http-get/gohttp.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/tangx/go-examples/utils"
 )
@@ -24,4 +25,12 @@ func httpGet(URL string) {
 	utils.PanicError(err)
 
 	fmt.Println("http resp Body: ", string(body))
+}
+
+// httpGetCustom 自动移 http 请求，并设置 3 秒超时时间。
+func httpGetCustom(url string) (resp *http.Response, err error) {
+	cli := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	return cli.Get(url)
 }
